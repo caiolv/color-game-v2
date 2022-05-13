69fb2081f233b5d76946240e64ccfbd1cb64b590
Caio Oliveira <caiolv@me.com>
2022-05-13 11:03:28 -0300

    add: compile css

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import { Provider } from 'react-redux';
 
 import store from '../store';
 
-import "../styles/main.scss";
+import "../styles/styles.css";
 
 import Header from "./Header";
 import Squares from "./Squares";
