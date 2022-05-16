6f105cb79d22f82d0154cdb34627eaf1ea7efc52
Caio Oliveira <caiolv@me.com>
2022-05-16 10:32:19 -0300

    fix: shadow on mobile

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -4,14 +4,14 @@ export const Container = styled.div`
     padding: 15px;
     background-color: ${props => props.color};
     transition: background-color 0.6s;
-    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.5);
+    box-shadow: 0px 3px 1px rgba(0, 0, 0, 0.2);
 
     h1 {
         font-weight: 500;
         font-size: clamp(2.75rem, 8vw, 6rem);//font-size: 2.75rem;
         color: #fef;
         text-align: center;
-        text-shadow: 4px 3px 0px #333, 8px 5px 0px rgba(0,0,0,0.15);
+        text-shadow: 0.3rem 0.19rem 0 #333, 0.6rem 0.3125rem 0 rgba(0,0,0,0.15);
 
     }
 
@@ -23,7 +23,7 @@ export const Container = styled.div`
         font-weight: 700;
         color: #fef;
         text-align: center;
-        text-shadow: 4px 3px 0px #333, 8px 5px 0px rgba(0,0,0,0.15);
+        text-shadow: 0.3rem 0.19rem 0 #333, 0.6rem 0.3125rem 0 rgba(0,0,0,0.15)
 
     }
 `;
